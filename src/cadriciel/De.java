09740e3b93318526222ade52252b7201fe414847
julien <julien.audet.1@ens.etsmtl.ca>
2018-03-04 15:49:23 -0500

    methode brasser

--- src/cadriciel/De.java
+++ src/cadriciel/De.java
@@ -1,11 +1,19 @@
 package cadriciel;
+import java.util.Random;
 
 public class De {
     private int nbrFaces ;
+    private static final int MAX = 6;
+    private static final int MIN = 1;
 
 
 
-    public void brasser() {
-        
+    public int brasser() {
+
+        Random ran = new Random();
+        return ran.nextInt(MAX-MIN+1)+MIN;
+
+
     }
 }
+
