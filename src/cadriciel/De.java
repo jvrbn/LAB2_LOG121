ddebf521e3572c48a4344dbdcd9dea5b12c2ac84
Philippe Paquette <philippe.paquette.5@ens.etsmtl.ca>
2018-03-05 00:45:10 -0500

    - Fait la classe De

--- src/cadriciel/De.java
+++ src/cadriciel/De.java
@@ -3,9 +3,23 @@ package cadriciel;
 public class De {
     private int nbrFaces ;
 
+    public De(int nbFaces) {
+        //Le nombre de face d'un dé ne peut pas être négatif ou zéro.
+        if(nbFaces < 1)
+            throw new IllegalArgumentException("Le nombre de faces d'un dé doit être supérieur ou égale à 1.");
 
+        nbrFaces = nbFaces;
+    }
 
-    public void brasser() {
-        
+    public int getNbFaces(){
+        return nbrFaces;
+    }
+
+    /**
+     * Génere un chiffre aléatoire entre 1 et le nombre de faces du dé.
+     * @return Un chiffre aléatoire.
+     */
+    public int brasser() {
+        return  (int)(Math.random() * nbrFaces + 1);
     }
 }
