67591bdbd878b5afcf983d6e6025797832c257c9
jvrbn <javierjr.beltran@gmail.com>
2018-03-08 10:35:15 -0500

    Jeu

--- src/cadriciel/Jeu.java
+++ src/cadriciel/Jeu.java
@@ -2,7 +2,7 @@ package cadriciel;
 
 public class Jeu {
 
-    private int nbTours = 0;
+    private int nbTours = 1;
 
     public Jeu(CollectionDes des, CollectionJoueurs joueurs, IStrategieRegles regles) {
 
