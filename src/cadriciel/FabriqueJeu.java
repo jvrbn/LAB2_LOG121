aa4b99afe4438efe7ea244d33cb341e957a911ad
Javier Beltran <javierjr.beltran@gmail.com>
2018-03-14 22:39:46 -0400

    Commentaire au debut des classes

--- src/cadriciel/FabriqueJeu.java
+++ src/cadriciel/FabriqueJeu.java
@@ -1,3 +1,24 @@
+/******************************************************
+ Cours:   LOG121
+ Session: A2013
+ Groupe:  01
+ Projet: Laboratoire #2
+ Étudiant(e)s:
+ Julien Audet
+ Javier Beltran
+ Philippe Paquette
+ Napoleon Echeverria
+
+ Professeur : Francis Cardinal
+ Nom du fichier: FabriqueJeu.java
+ Date créé: 2018-03-01
+ Date dern. modif. 2018-03-14
+ *******************************************************
+ Historique des modifications
+ *******************************************************
+ 2018-03-14 Finalisation de FabriqueJeu
+ *******************************************************/
+
 package cadriciel;
 
 /**
